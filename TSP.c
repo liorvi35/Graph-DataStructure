92f5f7726bb875d90cd275370bc8eb156f24c0a8
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-16 00:53:37 +0200

    Update TSP.c

--- TSP.c
+++ TSP.c
@@ -1,68 +1,40 @@
-int TSP(pnode head, int size) {
-    int *visited = calloc(size, sizeof(int));
-    int *path = calloc(size, sizeof(int));
-
-    for (int i = 0; i < size; i++) {
-        scanf("%d", &path[i]);
+int shortest_path_held_karp(int num_nodes, int *nodes_list, int num_of_specific_nodes, int **dist) {
+    // create a lookup table for storing the minimum distances
+    int **lookup_table = (int**)malloc(sizeof(int*) * (1 << num_of_specific_nodes));
+    for (int i = 0; i < (1 << num_of_specific_nodes); i++) {
+        lookup_table[i] = (int*)malloc(sizeof(int) * num_of_specific_nodes);
+        for (int j = 0; j < num_of_specific_nodes; j++) {
+            lookup_table[i][j] = INFINITE;
+        }
     }
-
-    int min_cost = INT_MAX;
-    do {
-        int cost = 0;
-        int prev = path[0];
-        visited[prev - 1] = 1;
-        for (int i = 1; i < size; i++) {
-            int current = path[i];
-            pnode start = head;
-            while (start) {
-                if (start->node_num == prev) {
-                    pedge e = start->edges;
-                    while (e) {
-                        if (e->endpoint->node_num == current) {
-                            cost += e->weight;
-                            break;
-                        }
-                        e = e->next;
-                    }
-                    break;
-                }
-                start = start->next;
+    // set the distance from a node to itself to 0
+    for (int i = 0; i < num_of_specific_nodes; i++) {
+        lookup_table[1 << i][i] = 0;
+    }
+    // fill the lookup table using dynamic programming
+    for (int i = 0; i < (1 << num_of_specific_nodes); i++) {
+        for (int j = 0; j < num_of_specific_nodes; j++) {
+            if (lookup_table[i][j] == INFINITE) {
+                continue;
             }
-            prev = current;
-            visited[prev - 1] = 1;
+            for (int k = 0; k < num_of_specific_nodes; k++) {
+                if (i & (1 << k)) {
+                    continue;
+                }
+                int new_dist = lookup_table[i][j] + dist[nodes_list[j]][nodes_list[k]];
+                lookup_table[i | (1 << k)][k] = min(lookup_table[i | (1 << k)][k], new_dist);
+            }
         }
-        if (cost < min_cost) {
-            min_cost = cost;
-        }
-        memset(visited, 0, size*sizeof(int));
-    } while (next_permutation(path, path + size));
-
-    free(visited);
-    free(path);
-    return min_cost;
-}
-
-void next_permutation(int *arr, int size) {
-    int i = size - 2;
-    while (i >= 0 && arr[i] >= arr[i + 1]) {
-        i--;
     }
-    if (i >= 0) {
-        int j = size - 1;
-        while (j > i && arr[j] <= arr[i]) {
-            j--;
-        }
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    // find the minimum distance
+    int min_distance = INFINITE;
+    for (int i = 0; i < num_of_specific_nodes; i++) {
+        min_distance = min(min_distance, lookup_table[(1 << num_of_specific_nodes) - 1][i]);
     }
-    reverse(arr + i + 1, arr + size);
-}
-
-void reverse(int *begin, int *end) {
-    while (begin < --end) {
-        int temp = *begin;
-        *begin++ = *end;
-        *end = temp;
+    // free the lookup table
+    for (int i = 0; i < (1 << num_of_specific_nodes); i++) {
+        free(lookup_table[i]);
     }
+    free(lookup_table);
+    return min_distance;
 }
