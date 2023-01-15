e174f86a36910e2b2f790e3c709e129a89d9f349
liorvi35 <liorvi35@gmail.com>
2023-01-15 19:54:32 +0200

    Update and rename S to dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -1,45 +1,58 @@
-int dijkstra(pnode head, int n1, int n2) {
+int dijkstra(pnode *head, int n1, int n2) 
+{
     pnode current = head;
-    while (current) {
+    while (current) 
+    {
         current->dist = INT_MAX;
         current->visited = 0;
         current = current->next;
     }
     current = head;
-    while (current) {
-        if(current->node_num == n1) {
+    while (current) 
+    {
+        if(current->node_num == n1) 
+        {
             current->dist = 0;
             break;
         }
         current = current->next;
     }
-    while (1) {
+    while (1) 
+    {
         int min_dist = INT_MAX;
         pnode min_node = NULL;
         current = head;
-        while (current) {
-            if (!current->visited && current->dist < min_dist) {
+        while (current) 
+        {
+            if (!current->visited && current->dist < min_dist) 
+            {
                 min_dist = current->dist;
                 min_node = current;
             }
             current = current->next;
         }
-        if (!min_node) {
+        if (!min_node) 
+        {
             break;
         }
         min_node->visited = 1;
         pedge e = min_node->edges;
-        while (e) {
-            if (e->endpoint->dist > min_node->dist + e->weight) {
+        while (e) 
+        {
+            if (e->endpoint->dist > min_node->dist + e->weight) 
+            {
                 e->endpoint->dist = min_node->dist + e->weight;
             }
             e = e->next;
         }
     }
     current = head;
-    while (current) {
-        if (current->node_num == n2) {
-            if (current->dist == INT_MAX) {
+    while (current) 
+    {
+        if (current->node_num == n2)
+         {
+            if (current->dist == INT_MAX) 
+            {
                 return -1;
             }
             return current->dist;
