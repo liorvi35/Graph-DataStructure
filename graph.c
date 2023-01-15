a6f5ce28c330b46cdea6a693b1f23c179e1b94ef
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-01-15 16:50:52 +0200

    Update graph.c
    Add delete

--- graph.c
+++ graph.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 #include "graph.h"
 
+void deleteGraph_cmd(pnode* head)
+{
+    pnode currNode = *head, nodeTmp = NULL;
+    pedge currEdge = NULL, edgeTmp = NULL;
+    while(currNode != NULL)
+    {
+        currEdge = currNode->edges;
+        while (currEdge != NULL)
+        {
+            edgeTmp = currEdge;
+            currEdge = currEdge->next;
+            free(edgeTmp); 
+        }
+        currNode->edges = NULL;
+        nodeTmp = currNode;
+        currNode = currNode->next;
+        free(nodeTmp);
+    }
+    *head = NULL;
+
+}
+
 void build_graph_cmd(pnode *head, pnode *tail, int nodes)
 {
     int i = 0;
